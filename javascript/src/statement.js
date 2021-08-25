66dc9347c886c60f38ae2848b747c67b6d5fcd29
Kirsten Nordine <kirstennordine@gmail.com>
2021-08-25 19:13:36 -0400

    formatting

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -3,18 +3,16 @@ function calculateCostForTragedy(perf) {
     if (perf.audience > 30) {
         costForPerformance += 1000 * (perf.audience - 30);
     }
-    return costForPerformance
-
+    return costForPerformance;
 }
 
 function calculateCostForComedy(perf) {
-            let costForPerformance = 30000;
-            if (perf.audience > 20) {
-                costForPerformance += 10000 + 500 * (perf.audience - 20);
-            }
-            costForPerformance += 300 * perf.audience;
-            return costForPerformance;
-
+    let costForPerformance = 30000;
+    if (perf.audience > 20) {
+        costForPerformance += 10000 + 500 * (perf.audience - 20);
+    }
+    costForPerformance += 300 * perf.audience;
+    return costForPerformance;
 }
 
 function calculateVolumeCreditsForTragedy(perf) {
@@ -39,38 +37,42 @@ const genreConfigurator = {
     comedy: {
         cost: calculateCostForComedy,
         volumeCredits: calculateVolumeCreditsForComedy,
-    }
-}
+    },
+};
 
 function calculateCost(perf, type) {
     if (!genreConfigurator[type]) {
-            throw new Error(`unknown type: ${type}`);
+        throw new Error(`unknown type: ${type}`);
     }
-    return genreConfigurator[type].cost(perf)
+    return genreConfigurator[type].cost(perf);
 }
 
 function calculateVolumeCredits(perf, play) {
-    return genreConfigurator[play['type']].volumeCredits(perf)
+    return genreConfigurator[play["type"]].volumeCredits(perf);
 }
 
-function statement (invoice, plays) {
+function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
-        { style: "currency", currency: "USD",
-            minimumFractionDigits: 2 }).format;
+    const format = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2,
+    }).format;
 
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
-        const costForPerformance = calculateCost(perf, play.type)
-        volumeCredits += calculateVolumeCredits(perf, play)
-        
+        const costForPerformance = calculateCost(perf, play.type);
+        volumeCredits += calculateVolumeCredits(perf, play);
+
         // print line for this order
-        result += ` ${play.name}: ${format(costForPerformance/100)} (${perf.audience} seats)\n`;
+        result += ` ${play.name}: ${format(costForPerformance / 100)} (${
+            perf.audience
+        } seats)\n`;
         totalAmount += costForPerformance;
     }
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
+    result += `Amount owed is ${format(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 }
